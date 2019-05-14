6ad13f88ebc045e7cad861a3a6fac5274bba667e
moseslucas <mo@moseslucas.com>
2019-05-14 16:23:13 +0800

    Slot machine 4 items

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import SlotMachine from 'react-native-slot-machine';
+
+type Props = {};
+export default class App extends Component<Props> {
+	constructor(props) {
+		super(props);
+    this.state = {
+      duration: 10000,
+      slot1: 1234,
+      slot2: 'hello',
+      slot3: '2351'
+    };
+	}
+
+	render() {
+		const symbols = ['🍏', '🍎', '🍐', '🍊', '🍋', '🍌']; // can't use emojies in SlotMachine because some of them are comprised of 2 chars
+		return (
+				<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <View>
+          </View>
+						<View style={{height: 200, justifyContent: 'space-between', alignItems: 'center'}}>
+              <SlotMachine
+                height={80}
+                width={80}
+                text={this.state.slot3}
+                range="0123450123450123450123450123450123450123"
+                renderContent={c => <Text style={{fontSize: 60}}>{symbols[c]}</Text>}
+                duration={this.state.duration}
+                slotInterval={3000}
+                styles={{
+                  container: {
+                    backgroundColor: '#fff'
+                  },
+                  slotWrapper: {
+                    backgroundColor: '#fff'
+                  },
+                  slotInner: {
+                    backgroundColor: '#fff'
+                  },
+                  innerBorder: {
+                    borderColor: '#fff'
+                  },
+                }}
+              />
+						</View>
+				</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
